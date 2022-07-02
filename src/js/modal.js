8b7eec8e9327b03ea4b5ad4eda1e98c38ee0fef2
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-07-02 17:07:22 +0300

    Done

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,53 +1,74 @@
 
-const btns = document.querySelectorAll('.modal__btn');
+const btns = document.querySelector('.modal__open');
 const modalOverlay = document.querySelector('.modal__card-overlay');
 const modalCard = document.querySelector('.modal__card');
 const closeBtn = document.querySelector('.modal__close');
 
 openModal();
-closeModal();
+
+
+
 
 
 function openModal() {
-    btns.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-            let path = e.currentTarget.getAttribute('data-path');
+       
+    btns.addEventListener('click', showModal);
+        function showModal(e) {
+        e.preventDefault();
+        let path = e.currentTarget.getAttribute('data-path');
+        modalCard.classList.remove('modal__card--active');
+        document.querySelector(`[data-target="${path}"]`).classList.add('modal__card--active');
+        modalOverlay.classList.add('modal__card-overlay--active');
+        closeBtn.addEventListener('click', closeByBtn);
+        modalOverlay.addEventListener('click', closeByOverlay);
+        window.addEventListener('keydown', closeByWindow);
+    };
+       
+ 
 
-            modalCard.classList.remove('modal__card--active');
-            document.querySelector(`[data-target="${path}"]`).classList.add('modal__card--active');
-            modalOverlay.classList.add('modal__card-overlay--active');
-        });
-    });
-    removeEventListener(`click`, openModal)
     
-}
-
-function closeModal() {
-    modalOverlay.addEventListener('click', (e) => {
+    
+    function closeByOverlay(e) {
         if (e.target === modalOverlay) {
+            modalOverlay.removeEventListener('click', closeByOverlay);
             modalOverlay.classList.remove('modal__card-overlay--active');
             modalCard.classList.remove('modal__card--active');
-        }
+            
+        }  
+    };
     
-    });
-    closeBtn.addEventListener('click', (e) => {
+    function closeByBtn(e) {
+        
         if (e.target === closeBtn) {
+            closeBtn.removeEventListener('click', closeByBtn);
             modalOverlay.classList.remove('modal__card-overlay--active');
             modalCard.classList.remove('modal__card--active');
-           
             console.log('close');
-        } 
+            
+        }
+    };
 
-    });
-    window.addEventListener('keydown', (e) => {
+    function closeByWindow(e) {
         if (e.keyCode === 27) {
+            window.removeEventListener('keydown', closeByWindow);
             modalOverlay.classList.remove('modal__card-overlay--active');
             modalCard.classList.remove('modal__card--active');
         }
-    });
+}
 
+
+
+
+
+
+
+
+ 
+    
 
 }
+
+
 
 
 
