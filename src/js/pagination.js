ec2c1224514bc7a08d122be165ca15c081d4117a
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-07-04 13:06:55 +0300

    added favicon and imports for pagination

---
+++ src/js/pagination.js
@@ -0,0 +1,8 @@
+import Pagination from 'tui-pagination';
+
+// const container = document.getElementById('tui-pagination-container');
+// const instance = new Pagination(container, { ... });
+
+// instance.getCurrentPage();
+
+export function createPagination() {}
