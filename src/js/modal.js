ffdfeeda4e9f3e6e2a6578e24eba7af4e3054004
Serhii Kushneruk <svkushneruk@ukr.net>
2022-07-05 22:22:07 +0300

    FT-17: fetchMovieDetails

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,75 @@
 const btns = document.querySelector('.movie-collection');
 const modalOverlay = document.querySelector('.modal__card-overlay');
 const modalCard = document.querySelector('.modal__card-content');
-const closeBtn = document.querySelector('.modal__close');
+let closeBtn = null;
 
 btns.addEventListener('click', showModal);
-function showModal(e) {
+async function showModal(e) {
   e.preventDefault();
   if (e.target !== btns) {
-    modalOverlay.classList.add('modal__card-overlay--active');
-    modalCard.classList.add('modal__card--active');
-    closeBtn.addEventListener('click', closeByBtn);
-    modalOverlay.addEventListener('click', closeByOverlay);
-    window.addEventListener('keydown', closeByWindow);
+    const movieId = e.target.closest('.movies__item').dataset.id;
+    modalCard.innerHTML = await fetchMovieDetails(movieId).then(data => {
+      const {
+        original_title,
+        vote_average,
+        vote_count,
+        poster_path,
+        popularity,
+        genres,
+        overview,
+      } = data;
+      console.log(data);
+      return `
+                <svg class="modal__close" width="14" height="14"><use href="/src/images/symbol-defs.svg#close"></use></svg>
+                <img src="https://image.tmdb.org/t/p/original${poster_path}" alt="${original_title.toUpperCase()}" class="modal__img" />
+                <div class="modal__info">
+                  
+                  <h2 class="modal__info-title">${original_title.toUpperCase()}</h2>
+                    
+                  <ul class="modal__info discription__modal">
+                      <li class="discription__modal-item">
+                        <p class="discription__modal-title">Vote / Votes</p>
+                        <p class="discription__modal-text"><span>${vote_average}</span> / ${vote_count}</p>
+                      </li>
+                      <li class="discription__modal-item">
+                        <p class="discription__modal-title">Popularity</p>
+                        <p class="discription__modal-text">${popularity.toFixed(
+                          1
+                        )}</p>
+                      </li>
+                      <li class="discription__modal-item">
+                        <p class="discription__modal-title">Original Title</p>
+                        <p class="discription__modal-text">${original_title.toUpperCase()}</p>
+                      </li>
+                      <li class="discription__modal-item">
+                        <p class="discription__modal-title">Genre</p>
+                        <p class="discription__modal-text">${checkGenreList(
+                          genres
+                        )}</p>
+                      </li>
+                  </ul>
+
+                  <div class="modal__info about__modal">
+                      <h4 class="about__modal-title">About</h4>
+                      <p class="about__modal-text">
+                        ${overview}
+                      </p>
+                  </div>
+                  <div class="modal__button">
+                      <button type="button" class="modal__btn-add modal__active-btn">add to Watched</button>
+                      <button type="button" class="modal__btn-add">add to queue</button>
+                      </div>
+                  </>
+                </div>
+            `;
+    });
   }
+  closeBtn = document.querySelector('.modal__close');
+  modalOverlay.classList.add('modal__card-overlay--active');
+  modalCard.classList.add('modal__card--active');
+  closeBtn.addEventListener('click', closeByBtn);
+  modalOverlay.addEventListener('click', closeByOverlay);
+  window.addEventListener('keydown', closeByWindow);
 }
 
 // let path = e.currentTarget.getAttribute('data-path');
@@ -41,5 +98,30 @@ function closeByWindow(e) {
     window.removeEventListener('keydown', closeByWindow);
     modalOverlay.classList.remove('modal__card-overlay--active');
     modalCard.classList.remove('modal__card--active');
+  }
+}
+
+async function fetchMovieDetails(id) {
+  return await fetch(
+    `https://api.themoviedb.org/3//movie/${id}?api_key=7fea517bd5b294dd7a1b57e94e2c1c68&language=en-US`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    });
+}
+
+function checkGenreList(genres) {
+  if (genres.length === 0) {
+    return '';
+  } else if (genres.length > 2) {
+    return `${genres[0].name}, ${genres[1].name}, Other`;
+  } else {
+    return `${[genres.map(genre => genre.name)].join(', ')}`;
   }
 }
