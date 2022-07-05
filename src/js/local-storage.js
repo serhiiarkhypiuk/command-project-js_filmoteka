1b195980a64d855f0ccd80899c16a93b2cfb4b92
Oleksii Horban <lehagorban93@gmail.com>
2022-07-05 22:38:53 +0300

    local-storage-FT-19
    я в першому коміті мабуть не той ключ підключив

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,20 +1,21 @@
 const queueBtn = document.querySelector("#btn-queue")
+let pageNumber = 1
 console.log(queueBtn);
 queueBtn.addEventListener("click", addToQueue)
 
 async function addToQueue() {
     const response = await fetchFilms()
     console.log(response);
-    console.log(response.genres);
-    // const filmsId = response.genres.map(genres => genres)
-   
+    console.log(response.results);
+    const filmsTitle = response.results.map(({title}) => title)
+   console.log(filmsTitle);
     // console.log(filmsId);
-    localStorage.setItem("data-queue", JSON.stringify(response.genres))
+    localStorage.setItem("data-queue", JSON.stringify(filmsTitle))
 }
 
 async function fetchFilms() {
    try {
-    const results = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=7fea517bd5b294dd7a1b57e94e2c1c68&language=en-US')
+    const results = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=7fea517bd5b294dd7a1b57e94e2c1c68&language=en-US&page=${pageNumber}`)
    const response = await results.json()
    console.log(response);
     return response
